5b9ad3515ba47a1f2a789d7119dc9767df118b28
Sachin Agarwal <sachin@bigbitbus.com>
2018-04-07 11:48:57 -0400

    Added source installed checker

--- stressng/files/compileinstall.sh
+++ stressng/files/compileinstall.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
+if [ -e /stressng_from_source_installed ];then
+  echo "libgit2+pygit2 already installed from source - /stressng_from_source_installed exists."
+  exit 0
+fi
 cd /tmp
 tar xf stress-ng-*
 cd /tmp/stress-ng-*
@@ -7,3 +11,4 @@ make
 cp stress-ng /usr/bin
 cd /tmp
 rm -rf /tmp/stress-*
+touch /stressng_from_source_installed
